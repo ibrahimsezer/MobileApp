1340aff370b647e8d22433e8531e177999fa181b
İBRAHİM SEZER <ibrahimszr16@gmail.com>
2022-03-17 01:08:06 +0300

    BSTv1.0.0
    İlk görsel düzen örneği yapıldı.

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -10,26 +10,34 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 import googletrans
 from googletrans import Translator
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.lang import Builder
+from kivy.core.window import Window
+from kivy.uix.image import Image
 import pandas as pd
 class MainWidget(Widget):
     pass
+
+
 #ÇEVİRİ MODÜLÜ
 translator = Translator()
-sentence = str(input("Cümle giriniz:"))
-example = translator.translate(sentence,dest="tr")
+#sentence = str(input("Cümle giriniz:"))
+#example = translator.translate(sentence,dest="en")
 
-print(example.src)
-"tr" # orjinal dil
-print(example.dest)
-"en" # çevrilen dil
-print(example.origin)
-"Olmak ya da olmamak"
-print(example.text)
-"To be or not to be"
+#print(example.src)
+#"tr" # orjinal dil
+#print(example.dest)
+#"en" # çevrilen dil
+#print(example.origin)
+#"Olmak ya da olmamak"
+#print(example.text)
+#"To be or not to be"
 #def callback(instance):
     #print('The button <%s> is being pressed' % instance.text)
 
 class TranslatorApp(App):
     pass
+
 
 TranslatorApp().run()
